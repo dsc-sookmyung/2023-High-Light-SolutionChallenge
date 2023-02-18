477290dc0e2f905dd55dfa11c5cc6d5bb47baf13
gdakate <gdakate1215@gmail.com>
2023-02-18 14:08:35 +0900

    백엔드 초기 세팅

---
+++ src/main/java/com/SollutionChallenge/HighLight/HighLightApplication.java
@@ -0,0 +1,13 @@
+package com.SollutionChallenge.HighLight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighLightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SollutionChallenge/HighLight/HighLightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SollutionChallenge.HighLight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
