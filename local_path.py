c60140d98293b64b98754776dcb2a367eb742767
sooking87 <sksohn01@naver.com>
2023-05-10 13:19:10 +0900

    [init]

--- local_path.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for filename in os.listdir("os_3_folder"):
-    # with open(os.path.join("os_3_folder", filename), 'r') as f:
-    #     text = f.read().decode('utf-8')
-    #     print(text)
-    print(filename)
