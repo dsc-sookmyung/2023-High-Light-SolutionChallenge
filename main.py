cd714ddf86b9966499726b05597a1a772aa7ad32
sooking87 <sksohn01@naver.com>
2023-05-24 22:36:04 +0900

    [feat] 배포 완료

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import json
 import requests
-from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from google.cloud import storage
-import json
+from PIL import Image
+import torch
 from googletrans import Translator
 from google.cloud import texttospeech
-
 
 FINAL_BUCKET = "cloud_storage_leturn"
 BUCKET = ""
@@ -25,7 +24,9 @@ image_folder_path = ''
 def download_json(event, context):
     # download json file from bucket
     global BUCKET, FILE_NAME, USER_ID, json_folder_path, json_filename, audio_folder_path, audio_full_filename, audio_one_filename, image_folder_path, file_no_extension, file_path
-
+    print('event: ', event)
+    print('context: ', context)
+    
     BUCKET = event['bucket']
     FILE_NAME = event['name'].split('/')[-1]
     file_path = event['name']
@@ -52,13 +53,13 @@ def download_json(event, context):
     print(download_data)
     for_making_caption(download_data)
 
-
 def gen_image_caption(img_url):
     processor = BlipProcessor.from_pretrained(
         "Salesforce/blip-image-captioning-base")
     model = BlipForConditionalGeneration.from_pretrained(
         "Salesforce/blip-image-captioning-base")
 
+    img_url = img_url
     raw_image = Image.open(requests.get(
         img_url, stream=True).raw).convert('RGB')
 
@@ -66,8 +67,30 @@ def gen_image_caption(img_url):
 
     out = model.generate(**inputs)
     image_caption = processor.decode(out[0], skip_special_tokens=True)
+    print(image_caption)
     return image_caption
+    
+def for_making_caption(data):
+    print("SUCCESS in for_making_caption")
+    for i in range(1, len(data) + 1):
+        count = str(i)
+        image = data[count]["image"]
+        for j in range(len(image)):
+            print("img_url: ", image[j]["img_url"])
+            img_idx = image[j]["img_idx"]
+            print("img_idx: ", img_idx)
+            image_caption = gen_image_caption(image[j]["img_url"])
+            print("image_caption: ", image_caption)
+            translator = Translator()
+            text = image_caption
+            result = translator.translate(text, dest='ko')
 
+            text_to_speech(
+                result.text, f"{image_folder_path}/{count}/{file_no_extension}_image_audio_{img_idx}.mp3")
+            image[j]["img_text"] = image_caption
+            image[j]["img_audio_url"] = f"https://storage.googleapis.com/{FINAL_BUCKET}/{image_folder_path}/{count}/{file_no_extension}_image_audio_{img_idx}.mp3"
+
+    prepare_upload_json(data, FINAL_BUCKET)
 
 def text_to_speech(text, path):
     print("SUCCESS in text_to_speech")
@@ -87,7 +110,7 @@ def text_to_speech(text, path):
         input=synthesis_input, voice=voice, audio_config=audio_config
     )
 
-    return response.audio_content
+    upload_audio(response.audio_content, path, f'{FINAL_BUCKET}')
 
 
 def upload_audio(mp3, path, load_bucket):
@@ -101,20 +124,22 @@ def upload_audio(mp3, path, load_bucket):
     blob.upload_from_string(mp3)
     print(f'File {source_file_name} uploaded to {path}.')
 
+def upload_json(data, path, load_bucket):
+    print("SUCCESS in upload_json")
+    print(data)
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(load_bucket)
+    blob = bucket.blob(path)
 
-def for_making_caption(data):
-    print("SUCCESS in for_making_caption")
+    blob.upload_from_string(json.dumps(data), content_type="application/json")
+    print('File uploaded to {}.'.format(path))
+    
+def prepare_upload_json(data, load_bucket):
+    print("SUCCESS in upload_json")
+    print(len(data))
     for i in range(1, len(data) + 1):
         count = str(i)
-        image = data[count]["image"]
-        for j in range(len(image)):
-            print(image[j]["img_url"])
-            img_idx = image[j]["img_idx"]
-            image_caption = gen_image_caption(image[j]["img_url"])
-            print(image_caption)
-            translator = Translator()
-            result = translator.translate(image_caption, dest='ko')
-            text_to_speech(
-                result.text, f"{image_folder_path}/{count}/{file_no_extension}_image_audio_{img_idx}.mp3")
-            image[j]["img_text"] = image_caption
-            image[j]["img_audio_url"] = f"https://storage.googleapis.com/{FINAL_BUCKET}/{image_folder_path}/{count}/{file_no_extension}_image_audio_{img_idx}.mp3"
+        path = f"{json_folder_path}/{count}/{file_no_extension}_{count}.json"
+        upload_json(data[str(i)], path, load_bucket)
+        print('File uploaded to {}.'.format(
+            f"{json_folder_path}/{count}/{file_no_extension}_{count}.json"))
