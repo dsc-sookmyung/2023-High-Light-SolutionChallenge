827952136d522defa643d9c844a22be2228f2eda
Yujin Son <sonyj0430@sookmyung.ac.kr>
2023-06-04 19:59:20 +0900

    add:>image caption 정보 받아오기

--- backend/src/main/java/com/SollutionChallenge/HighLight/File/ImageInfoDto.java
+++ backend/src/main/java/com/SollutionChallenge/HighLight/File/ImageInfoDto.java
@@ -10,5 +10,6 @@ import lombok.*;
 public class ImageInfoDto {
     private Long img_idx;
     private String img_url;
-    private String audio_url;
+    private String img_text;
+    private String img_audio_url;
 }
